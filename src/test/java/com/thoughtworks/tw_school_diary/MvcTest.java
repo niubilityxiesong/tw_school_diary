587e8fa2e6c0612cade836957e18e389a3f8190b
niubilityxiesong <563527588@qq.com>
2018-09-03 15:23:11 +0800

    [Xin QI & Song XIE]feat: add test-database

--- src/test/java/com/thoughtworks/tw_school_diary/MvcTest.java
+++ src/test/java/com/thoughtworks/tw_school_diary/MvcTest.java
@@ -1,17 +1,25 @@
 package com.thoughtworks.tw_school_diary;
 
+import com.github.database.rider.core.api.configuration.DBUnit;
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.spring.api.DBRider;
 import com.thoughtworks.tw_school_diary.controller.DiaryController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
-@SpringBootTest(classes = TwSchoolDiaryApplication.class)
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = TwSchoolDiaryApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@DBRider
+@DBUnit(caseSensitiveTableNames = true)
+@ActiveProfiles("test")
+@DataSet("diary.yml")
 public abstract class MvcTest {
 
     @Autowired
